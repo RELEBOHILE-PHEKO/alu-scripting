e99673693c4c75c22f700e11b2cfcafaf9074a0a
Relebohile Pheko <145283035+GLADYS-ARCH@users.noreply.github.com>
2024-07-04 21:28:58 +0200

    Merge remote-tracking branch 'origin/master'

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,16 +5,9 @@ Contains the number_of_subscribers function
 import requests
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'linux:alu-scripting:v1.0.0 (by /u/Mpho_19)'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception as e:
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'linux:alu-scripting:v1.0.0 (by /u/Mpho_19)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
