2e5b01af43ff6f4571147f47eb2dcc2146acf46d
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-07-05 10:25:40 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,13 +8,6 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        int: The number of subscribers for the subreddit. Returns 0 if the subreddit
-             is invalid or if an error occurs.
     """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
