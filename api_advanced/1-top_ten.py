31dbc551fd56db6857dce8438c1d90a9430b26ee
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-07-06 14:05:09 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-"""
-Module for querying the Reddit API and printing top posts.
-"""
-
+"""" Top Ten Limit"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    from the given subreddit.
+    """"top ten"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    my_headers = {
+        "User-Agent": "api-advanced/project"
+        }
 
-    Args:
-        subreddit (str): The name of the subreddit to query.
+    response = requests.get(URL, headers=my_headers)
+    # raw_response = response.json()['data']['children']
 
-    Returns:
-        None
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'API-Advanced'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except Exception:
+    if response.status_code != 200:
         print(None)
+    else:
+        json_response = response.json()
+        posts = json_response['data']['children']
+        [print(post.get('data').get('title')) for post in posts]
