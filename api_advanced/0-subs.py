fd2971036262e001b4885e5b78e3a5dde817a706
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-07-06 13:37:17 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,12 +3,29 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers if the request is successful, otherwise 0.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={"User-Agent": "API-Advanced"}
-    subs = r.get("data", {}).get("subscribers", 0)
+    
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {"User-Agent": "API-Advanced"}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return 0
+
+    try:
+        subs = response.json().get("data", {}).get("subscribers", 0)
+    except ValueError:
+        return 0
+
     return subs
