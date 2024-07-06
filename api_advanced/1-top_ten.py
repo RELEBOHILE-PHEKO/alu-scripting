e7a169effdba7cecd5c6b7001141df1cbae1c8ee
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-07-06 14:46:16 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Top Ten Posts from Reddit"""
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """first 10 hot posts from a subreddit."""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    my_headers = {
-        "User-Agent": "API-Advanced"
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "API -Advanced"
     }
-
-    try:
-        response = requests.get(URL, headers=my_headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print("None")
-    except requests.RequestException as e:
-        print(f"Error fetching subreddit posts: {e}")
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
