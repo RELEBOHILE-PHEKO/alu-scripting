8e8e6f2ad16e15d49dd7d730f8a1a78ccade1655
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-07-06 14:14:34 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -10,12 +10,16 @@ def top_ten(subreddit):
         "User-Agent": "API-Advanced"
         }
 
-    response = requests.get(URL, headers=my_headers)
-    # raw_response = response.json()['data']['children']
+  response = requests.get(URL, headers=headers)
 
     if response.status_code != 200:
-        print(None)
-    else:
+        print(f"Failed to fetch posts: Status Code {response.status_code}")
+        return
+    
+    try:
         json_response = response.json()
         posts = json_response['data']['children']
-        [print(post.get('data').get('title')) for post in posts]
+        for post in posts:
+            print(post['data']['title'])
+    except KeyError as e:
+        print(f"Failed to parse response: Missing key {e}")
